3ac0b3495c13bd89ffe2d8aa0ee2e60b67bf03a5
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-01 12:24:10 +1100

    Complete createGrid functionality

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const container1 = document.querySelector(".container");
 
 function createGrid(width){
-    for(i=1;i<=width;i++){
-        let square = document.createElement("div");
-        squareWidth = 800 / width;
-        console.log(squareWidth);
-        square.style.width = `${squareWidth}px`;
-        square.style.border = "solid 2px black";
-        square.style.cssText = "background-color:yellow; height:20px;"
-        container1.appendChild(square);
+    for(j=1;j<=width;j++){
+        for(i=1;i<=width;i++){
+            const square = document.createElement("div");
+            squareWidth = 700 / width;
+            console.log(squareWidth);
+            square.style.width = `${squareWidth}px`;
+            console.log(square.style.width)
+            square.style.border = "solid 0.01px grey";
+            square.style.backgroundColor = "yellow"
+            square.style.height = `${squareWidth}px`;
+            container1.appendChild(square);
+            square.style.padding = "0px"
+            square.style.margin = "0px"
+            
+        }
+        
     }
 }
 
-createGrid(16);
+createGrid(140);
